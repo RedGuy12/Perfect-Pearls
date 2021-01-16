370d56f3e2aecaf400f6c90cbceaa04a09ef9a93
Paul Reid <paul@reid-family.org>
2021-01-16 16:18:05 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-window.addEventListener("blur",()=>window.location.href = window.location.href.replace("perfect-pearls.glitch.me","redguy12.github.io/perfect-pearls/sleeping?url="));
+setInterval(() => { 
+	if(document.hasFocus()) {
+		window.location.href = window.location.href.replace("perfect-pearls.glitch.me","redguy12.github.io/perfect-pearls/sleeping?url=");
+	}
+}, 100);
