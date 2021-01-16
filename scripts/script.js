abe1a97cb4c88d0278b04d42caadb5d4e02521a7
Paul Reid <paul@reid-family.org>
2021-01-16 17:55:23 -0600

    hmmm

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,5 @@ setInterval(() => {
 		window.location.href = `https://redguy12.github.io/perfect-pearls/sleeping?url=${window.location.pathname}&y=${document.documentElement.scrollTop || document.body.scrollTop}&x=${document.documentElement.scrollLeft || document.body.scrollLeft}`;
 	}
 }, 100);
-document.documentElement.scrollTop = document.body.scrollTop = new URLSearchParams(window.location.search).get('y');
-document.documentElement.scrollLeft = document.body.scrollLeft = new URLSearchParams(window.location.search).get('x');
+document.documentElement.scrollTop = new URLSearchParams(window.location.search).get('y');
+document.documentElement.scrollLeft = new URLSearchParams(window.location.search).get('x');
