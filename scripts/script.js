21fcc7e6ec78ffbe1083703c0caa5183f194d263
Paul Reid <paul@reid-family.org>
2021-01-19 11:42:48 -0600

    oof forgot to push it to the pr

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 setInterval(() => {
 	if (!document.hasFocus()) {
-		window.location.href = `https://redguy12.github.io/perfect-pearls/sleeping?url=${window.location.pathname}&y=${document.documentElement.scrollTop || document.body.scrollTop}&x=${document.documentElement.scrollLeft || document.body.scrollLeft}`;
+		window.location.href = `https://redguy12.github.io/perfect-pearls/sleeping?url=${encodeURIComponent(window.location.pathname+window.location.search)}&y=${document.documentElement.scrollTop || document.body.scrollTop}&x=${document.documentElement.scrollLeft || document.body.scrollLeft}`;
 	}
 }, 100);
 document.documentElement.scrollTop = document.body.scrollTop = parseFloat(new URLSearchParams(window.location.search).get('y'));
